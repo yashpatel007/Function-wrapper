401da2f999f19f7dccb71ca376de90b94bc4f72a
LuckyLittleMonster <hqin4@binghamton.edu>
2019-11-10 11:27:27 -0500

    initial code

---
+++ rvalue_references/MyInt.hpp
@@ -0,0 +1,24 @@
+#ifndef MY_INT_HPP
+#define MY_INT_HPP
+
+
+
+#include <iostream>
+
+class MyInt {
+        friend std::ostream &operator<<(std::ostream &os, const MyInt &mi) {
+            return os << mi.i;
+        }
+    public:
+        MyInt() { count++; }
+        MyInt(const MyInt &mi) : i(mi.i) { count++; }
+        MyInt(int i) : i(i) { count++; }
+    private:
+        int i;
+    public:
+        static std::size_t count;
+};
+
+
+
+#endif
